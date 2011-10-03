af388622b3d3b753a2fdaea99cf27aba2bf61f16
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2011-10-02 23:24:44 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Resume::Application.config.secret_token = 'ed0f85f7745c40b3a6820edeb492c7ee7b63a288378b135b78e0e31aa65fa2e453267b8f929e5bcad7360043fa9c645d4c62ff413d60ba374ce3f2899dbddae7'
---
+++ public/javascripts/application.js
@@ -0,0 +1,5 @@
+jQuery(function($){
+  $("body").noisy({opacity : .1});
+
+  $("a").attr("target","_blank");
+});
